a58249d944257d0a1a669a7a0a51617865e05c2a
Sara Nolen <nolensara1@gmail.com>
2020-12-13 18:35:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese(types)
+  
+  types.find{|cheese| cheese.include?(cheese_types)}
   cheese_types = ["cheddar", "gouda", "camembert"]
-  types.find{|cheese| cheese == cheese_types}
 end
