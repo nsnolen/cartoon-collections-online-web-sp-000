855d20b734c4cfb186a2e842a8872a693d0fdc94
Sara Nolen <nolensara1@gmail.com>
2020-12-13 18:07:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ end
 def find_the_cheese(types)
   cheese_types = ["cheddar", "gouda", "camembert"]
   types.collect{|cheese| cheese.include?(cheese_types)}
-  
+
   if cheese
     "#{cheese}"
   else
